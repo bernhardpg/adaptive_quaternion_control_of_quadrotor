2041ab8610fe8c78a5a93507d9501304637641c7
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2020-04-16 08:58:43 -0400

    Implement baseline controller

--- include/controller/adaptive_controller.h
+++ include/controller/adaptive_controller.h
@@ -19,21 +19,32 @@ namespace controller {
     public:
       AdaptiveController(ros::NodeHandle *nh);
     private:
+      // ROS
       ros::NodeHandle nh_;
-
       ros::Subscriber odom_subscriber_;
       ros::Publisher command_publisher_;
 
+      // Signals
       // State
-      Eigen::Quaterniond q_;
-      Eigen::Vector3d w_;
+      Eigen::Quaterniond q_; // Rotation from body to inertial frame
+      Eigen::Vector3d w_; // Angular velocity between body and inertial frame
       // Errors
       Eigen::Quaterniond q_e_; // From body to command frame
       Eigen::Vector3d w_bc_; // Between body and command frame, given in body frame
       // Command
       Eigen::Quaterniond q_c_;
       Eigen::Vector3d w_c_; // Given in command frame
+      Eigen::Vector3d w_c_dot_;
 
+      // Model parameters
+      Eigen::Matrix3d J_; // Inertia matrix
+
+      // Controller
+      input_t input_;
+      double k_q_;
+      double k_w_;
+
+      // Functions
       void init();
       void calculateErrors();
       void computeInput();
@@ -41,6 +52,11 @@ namespace controller {
       void odomCallback(const nav_msgs::OdometryConstPtr &msg);
 
       double saturate(double v, double min, double max); // TODO move somewhere else
+      // TODO move somewhere else
+      Eigen::Matrix3d cross_map(Eigen::Vector3d v);
+      Eigen::Vector3d vee_map(Eigen::Matrix3d v_hat); // Inverse of cross_map
+      Eigen::Vector3d quat_log_v(Eigen::Quaterniond q);
+      Eigen::Quaterniond quat_plus_map(Eigen::Quaterniond q);
 
   };
 }
--- src/controller/adaptive_controller.cpp
+++ src/controller/adaptive_controller.cpp
@@ -15,15 +15,27 @@ namespace controller {
 
   void AdaptiveController::init()
   {
+    // Controller params
+    k_q_ = 1.0;
+    k_w_ = 1.0;
+
+    // Initialize model parameters
+    J_ = Eigen::Matrix3d::Identity();
+
     // Initialize variables
     q_ = Eigen::Quaterniond::Identity();
-    w_ << 0.0, 0.0, 0.0;
+    w_ = Eigen::Vector3d::Zero();
     q_e_ = Eigen::Quaterniond::Identity();
-    w_bc_ << 0.0, 0.0, 0.0;
+
+    w_bc_ = Eigen::Vector3d::Zero();
 
     // Set command signal
     q_c_ = Eigen::Quaterniond::Identity();
-    w_bc_ = Eigen::Vector3d::Zero();
+    w_c_ = Eigen::Vector3d::Zero();
+    w_c_dot_ = Eigen::Vector3d::Zero();
+
+    //height_c = 2.0;
+
   }
 
   void AdaptiveController::odomCallback(
@@ -32,6 +44,8 @@ namespace controller {
   {
     // Attitude
     q_ = Eigen::Quaterniond(msg->pose.pose.orientation.w,
+  
+
                             msg->pose.pose.orientation.x,
                             msg->pose.pose.orientation.y,
                             msg->pose.pose.orientation.z);
@@ -39,30 +53,43 @@ namespace controller {
     // Angular velocity
     w_ << msg->twist.twist.angular.x,
           msg->twist.twist.angular.y,
+
           msg->twist.twist.angular.z; // TODO should this be negative?
 
     // Controll loop
     calculateErrors();
     computeInput();
-
+    publishCommand();
   }
+
 
   void AdaptiveController::calculateErrors()
   {
+    // Calulate attitude error
+  
     q_e_ = q_c_ * q_;
+    // Calculate angular velocity error
     w_bc_ = w_ - q_e_.conjugate()._transformVector(w_c_);
 
     //std::cout << q_e_.w() << std::endl << q_e_.vec() << std::endl << std::endl;
     //std::cout << w_bc_ << std::endl << std::endl;
   }
 
+
   void AdaptiveController::computeInput()
   {
-      
-  
-  
+    // Baseline controller
+    Eigen::Vector3d cancellation_terms = cross_map(w_) * J_ * w_
+      + J_ * (w_c_dot_
+          + cross_map(w_c_) * w_c_);
+
+    // TODO change q_e_ to plus map
+    Eigen::Vector3d feedforward_terms = - k_q_ * quat_log_v(q_e_) - k_w_ * w_bc_;
+
+    input_.tau = cancellation_terms + feedforward_terms;
   }
 
+  
   void AdaptiveController::publishCommand()
   {
     rosflight_msgs::Command command;
@@ -71,7 +98,8 @@ namespace controller {
     command.mode = rosflight_msgs::Command::MODE_ROLLRATE_PITCHRATE_YAWRATE_THROTTLE;
     
     // F from 0 to 1, scale by max thrust
-    command.F = 0;
+    command.F = 0.6;
+    // TODO input here, but it must be scaled
     command.x = 0;
     command.y = 0;
     command.z = 0;
@@ -86,6 +114,38 @@ namespace controller {
         );
 
     return v;
+  }
+
+  // Note: also called hat map in litterature
+  Eigen::Matrix3d AdaptiveController::cross_map(Eigen::Vector3d v)
+  {
+    Eigen::Matrix3d v_hat;
+    v_hat << 0, -v(2), v(1),
+            v(2), 0, -v(0),
+          -v(1), v(0), 0;
+    return v_hat;
+  }
+
+  // Opposite of hat map
+  Eigen::Vector3d AdaptiveController::vee_map(Eigen::Matrix3d v_hat)
+  {
+    Eigen::Vector3d v;
+    v << v_hat(2,1), v_hat(0,2), v_hat(0,1);
+    k_q_ = 1.0;
+    return v;
+  }
+
+  // Returns the direct map of the quaternion logarithm to R^3 (instead of R^4)
+  Eigen::Vector3d AdaptiveController::quat_log_v(Eigen::Quaterniond q)
+  {
+    Eigen::AngleAxis<double> aa(q);
+    return (aa.angle() / 2) * aa.axis();
+  }
+
+  // Returns the short rotation quaternion with angle <= pi
+  Eigen::Quaterniond AdaptiveController::quat_plus_map(Eigen::Quaterniond q)
+  {
+    return q.w() >= 0 ? q : Eigen::Quaterniond(-q.w(), q.x(), q.y(), q.z());
   }
 
 } // namespace controller
